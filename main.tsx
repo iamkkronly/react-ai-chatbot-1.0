6856ddc9e1216a460bfc46d0c030b851b8baea92
Kaustav Ray <kkray1345@gmail.com>
2025-08-05 12:10:46 +0530

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -9,12 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);          </button>
-        </form>
-      </div>
-    </div>
-  );
-}
-
-const root = createRoot(document.getElementById("root")!);
-root.render(<ChatBot />);
+);
